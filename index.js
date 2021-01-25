80235a1e192eb8c52f341ab6943acd756e331d33
DominickDeJesus <dominick.dejesus@gmail.com>
2021-01-25 16:39:33 -0500

    cadded cron job for every hour

--- index.js
+++ index.js
@@ -1,34 +1,64 @@
 const puppeteer = require("puppeteer"),
-  $ = require("cheerio"),
-  URL =
-    "https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440",
-  TAG = `data-sku-id="6429440"`;
-//require("dotenv").config();
+	$ = require("cheerio"),
+	URL =
+		"https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440",
+	TAG = `data-sku-id="6429440"`,
+	cron = require("node-schedule"),
+	rule = new cron.RecurrenceRule();
+rule.hour = 1;
+rule.minute = 0;
 
-const checkStock = async (sourceHTML) => {
-  try {
-    const browser = await puppeteer.launch();
-    console.log("browser created");
-    const page = await browser.newPage();
-    console.log("window created");
-    await page.goto(URL, {
-      waitUntil: "load",
-      // Remove the timeout
-      timeout: 0,
-    });
-    console.log("loading page");
-    const html = await page.content();
+//Entry function
+(async () => {
+	try {
+		const browser = await puppeteer.launch({ headless: true });
+		console.log("[", new Date(), "] Browser created");
+		const page = await browser.newPage();
+		console.log("[", new Date(), "] Window created");
+		await page.goto(URL, {
+			waitUntil: "load",
+			// Remove the timeout
+			timeout: 0,
+		});
 
-    // trackObj.url = $("audio", html).attr("src");
-    // trackObj.title = $(`button[${TAG}]`, html).text();
-    const trackObj = $(`button[${TAG}]`, html).text();
-    //  const testTrack = await Track.findOne({ title: trackObj.title });
-    //if (testTrack) throw Error("Track already in database!");
-    console.log(trackObj);
-    return trackObj;
-  } catch (error) {
-    console.log(error);
-  }
-};
+		if (await isInStock(page, TAG)) {
+			sendTextNotification();
+		}
+		console.log("[", new Date(), "] Page reloading");
+		await page.reload();
 
-checkStock(URL).then();
+		cron.scheduleJob(rule, async function () {
+			if (await isInStock(page, TAG)) {
+				sendTextNotification();
+			}
+			console.log("[", new Date(), "] Page reloading");
+			await page.reload();
+		});
+
+		await browser.close();
+	} catch (error) {
+		console.log(error);
+		await browser.close();
+	}
+})();
+
+async function isInStock(page, tag) {
+	try {
+		console.log("[", new Date(), "] Loading page");
+		const html = await page.content();
+		const buttonText = $(`button[${tag}]`, html).text();
+		if (buttonText.toLocaleLowerCase() === "sold out") {
+			console.log("[", new Date(), "] Out of stock!");
+			return false;
+		}
+		console.log("[", new Date(), "] In stock!");
+		return true;
+	} catch (error) {
+		console.log(error);
+		return false;
+	}
+}
+
+function sendTextNotification() {
+	console.log("[", new Date(), "] Text Sent");
+}
