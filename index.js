cad5fb774d4a9feaa1a1ac98f3e0a83965e275f9
DominickDejesus <34385118+DominickDejsus@users.noreply.github.com>
2021-04-18 23:25:59 -0400

    added name to constructor

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const GPU =
 	"https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440";
 const PS5 =
 	"https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149";
-const sc = new SupplyChecker(PS5);
+const sc = new SupplyChecker(PS5, "Playstation");
 
 sc.init()
 	.then(() =>
